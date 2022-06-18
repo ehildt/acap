ea99112f224022f333d33eb2c8bb9ff4a6d99503
Eugen Hildt <eugen.hildt@posteo.de>
2022-06-18 16:10:56 +0200

    feat: init auths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from '@/auth/auth.module';
 import { ConfigManagerModule } from '@/config-manager/config-manager.module';
 import { ConsoleLogger, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -8,6 +9,7 @@ import { AppConfigRegistry } from './configs/app-config-registry.dbs';
 
 @Module({
   imports: [
+    AuthModule,
     ConfigManagerModule,
     ConfigModule.forRoot({
       cache: true,
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  async signup(req: any) {
+    return req;
+  }
+  async signin(req: any) {
+    return req;
+  }
+  async logout(req: any) {
+    return req;
+  }
+  async refresh(req: any) {
+    return req;
+  }
+}
