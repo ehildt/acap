c0b0ef46a7952acc56d75c45319201e67d0cc53a
Eugen Hildt <eugen.hildt@posteo.de>
2022-06-19 20:35:09 +0200

    wip: removed unused files

--- src/auth-manager/decorators/controller.decorators.ts
+++
@@ -1,5 +0,0 @@
-import { Body, ParseArrayPipe } from '@nestjs/common';
-import { AuthManagerSignupReq } from '../dtos/auth-manager-signup-req.dto';
-
-export const SignupBody = () =>
-  Body(new ParseArrayPipe({ items: AuthManagerSignupReq }));
