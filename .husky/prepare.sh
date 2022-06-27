0fcd53555a68efc3a187687d51462ea3b81c8a53
Eugen Hildt <eugen.hildt@posteo.de>
2022-06-27 16:37:19 +0200

    wip: gittyleaks

--- .husky/prepare.sh
+++ .husky/prepare.sh
@@ -94,7 +94,7 @@ check_licenses() {
 }
 
 gittyleaks_scan() {
-    local GITTYLEAKS=$(~/.local/bin/gittyleaks -b -e $ ./testy)
+    local GITTYLEAKS=$(~/.local/bin/gittyleaks -b -e $ $(cat testy))
     if [ ! $(echo "$GITTYLEAKS" | grep -o "No matches." | wc -m) -eq 12 ]; then
         info "gittyleaks_scan.. $(redfy error)"
         exit 1
