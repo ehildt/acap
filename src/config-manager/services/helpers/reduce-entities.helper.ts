67751a27c7c9dbd8c2c8f001abe5b8fe7cd0dcc9
Eugen Hildt <eugen.hildt@gmail.com>
2023-03-27 11:51:05 +0200

    feat/0000 ncu and migration (#66)
    * Dev (#65)
    
    * chore: dedupe
    
    * 0.7.1
    
    * fix: typo in pass-through-pubsup
    
    * 0.7.2
    
    * fix/0001 edit dockerfile (#62)
    
    * Dev => Master (#61)
    
    * chore: dedupe
    
    * 0.7.1
    
    * fix: typo in pass-through-pubsup
    
    * 0.7.2
    
    ---------
    
    Co-authored-by: Eugen Hildt <eugen.hildt@posteo.de>
    
    * fix: including config.yml in dist
    
    * chore(ncu): update
    
    * 0.7.3
    
    * chore(npm): dedupe
    
    ---------
    
    Co-authored-by: Eugen Hildt <eugen.hildt@posteo.de>
    
    * fix: CONFIG_MANAGER_RESOLVE_ENV undefined check
    
    * 0.7.4
    
    * feat: added user, pass to redis & publisher
    
    * chore(ncu): update
    
    * 0.7.5
    
    * Merge
    
    ---------
    
    Co-authored-by: Eugen Hildt <eugen.hildt@posteo.de>
    
    * chore(ncu): update & migration
    
    * chore(ncu): skipping typescript:5
    
    * 0.7.6
    
    ---------
    
    Co-authored-by: Eugen Hildt <eugen.hildt@posteo.de>
    Co-authored-by: Eugen Hildt <eugen.hildt@litlbyte.com>

--- src/config-manager/services/helpers/reduce-entities.helper.ts
+++ src/config-manager/services/helpers/reduce-entities.helper.ts
@@ -1,18 +1,16 @@
-import { LeanDocumentOrArray } from 'mongoose';
-
 import { ConfigManagerConfigsDocument } from '@/config-manager/schemas/configs.schema';
 
 import { challengeConfigValue } from './challenge-config-source.helper';
 
 const entityReducer = (
   previous: Record<string, unknown>,
-  document: LeanDocumentOrArray<ConfigManagerConfigsDocument>,
+  document: ConfigManagerConfigsDocument,
   resolveEnv: boolean,
 ) => ({
   ...previous,
   [document.configId]: challengeConfigValue(document.value, resolveEnv),
 });
 
-export function reduceEntities(resolveEnv: boolean, documents?: LeanDocumentOrArray<ConfigManagerConfigsDocument[]>) {
+export function reduceEntities(resolveEnv: boolean, documents?: ConfigManagerConfigsDocument[]) {
   return documents?.reduce((acc, val) => entityReducer(acc, val, resolveEnv), {});
 }
