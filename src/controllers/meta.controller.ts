7eb17728a1709f924e988dcd044552049ad79077
Eugen Hildt <eugen.hildt@litlbyte.com>
2023-07-11 10:39:23 +0200

    chore: merge(master,dev)

--- src/controllers/meta.controller.ts
+++ src/controllers/meta.controller.ts
@@ -2,7 +2,7 @@ import { Controller } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { GetMeta } from '@/decorators/controller.method.decorators';
-import { ParamMeta } from '@/decorators/controller.parameter.decorators';
+import { ParamSource } from '@/decorators/controller.parameter.decorators';
 import { QuerySkip, QueryTake } from '@/decorators/controller.query.decorators';
 import { OpenApi_GetMeta } from '@/decorators/open-api.controller.decorators';
 import { MetaService } from '@/services/meta.service';
@@ -16,8 +16,8 @@ export class MetaController {
 
   @GetMeta()
   @OpenApi_GetMeta()
-  async getRealmMeta(@ParamMeta() meta: META, @QueryTake() take: number, @QuerySkip() skip: number) {
-    if (meta === 'realms') return await this.metaService.getRealmMeta(take, skip);
-    if (meta === 'schemas') return await this.metaService.getSchemaMeta(take, skip);
+  async getRealmMeta(@ParamSource() metaSource: META, @QueryTake() take: number, @QuerySkip() skip: number) {
+    if (metaSource === 'realms') return await this.metaService.getRealmMeta(take, skip);
+    if (metaSource === 'schemas') return await this.metaService.getSchemaMeta(take, skip);
   }
 }
