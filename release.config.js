b6db198b6d87d7acb9d8142e8412bb668ac62bb3
Eugen Hildt <eugen.hildt@litlbyte.com>
2023-09-20 12:51:09 +0200

    feat: semantic-release

--- release.config.js
+++ release.config.js
@@ -2,13 +2,11 @@ module.exports = {
   branches: [
     'main',
     'dev',
-    'feat/*'
   ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    // '@semantic-release/npm',
     '@semantic-release/github',
     '@semantic-release/git',
   ],
