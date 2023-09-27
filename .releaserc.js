8a8767c7059caf570fe98120a809a5b52bf04ae1
Eugen Hildt <eugen.hildt@litlbyte.com>
2023-09-27 23:23:37 +0200

    chore(release): 1.1.0

--- .releaserc.js
+++ .releaserc.js
@@ -64,7 +64,7 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['package.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ]
   ],
