bd852ee539798e6aa4bf5ed5247d88237dc3e9b7
Eugen Hildt <eugen.hildt@litlbyte.com>
2023-10-08 21:11:13 +0200

    fix: build target for acap-client

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,8 +1,8 @@
-export { Button } from '@/button/Button';
-export { FileSelector } from '@/file-selector/FileSelector';
-export { PageMenu } from '@/page-menu/PageMenu';
-export { PageMenuItem } from '@/page-menu/PageMenuItem';
-export { PageSwitcher } from '@/page-switcher/PageSwitcher';
-export { Scrollbar } from '@/scrollbar/Scrollbar';
-export { SelectBox } from '@/select-box/SelectBox';
-export { Slider } from '@/slider/Slider';
+export { Button } from './button/Button';
+export { FileSelector } from './file-selector/FileSelector';
+export { PageMenu } from './page-menu/PageMenu';
+export { PageMenuItem } from './page-menu/PageMenuItem';
+export { PageSwitcher } from './page-switcher/PageSwitcher';
+export { Scrollbar } from './scrollbar/Scrollbar';
+export { SelectBox } from './select-box/SelectBox';
+export { Slider } from './slider/Slider';
