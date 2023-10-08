eef6af4a63d8ae823d431b8393fad4850409f22b
Eugen Hildt <eugen.hildt@litlbyte.com>
2023-10-08 16:44:16 +0200

    chore: updated vite.config.ts (build)

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -2,33 +2,51 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(() => ({
-  plugins: [tsConfigPaths(), react()],
-  envDir: './env',
-  build: {
-    lib: {
-      entry: './index.ts', // Path to your library entry file
-      name: '@acap/ui-react', // Global name for your library
-      fileName: 'acap-ui-react', // Output file name for your library
-      formats: ['es'] as any,
-    },
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase' as any,
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './test-setup.ts',
-    coverage: {
-      reporter: 'json',
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+  const productionConfig = isProduction
+    ? {
+        cssMinify: true,
+        minify: true,
+      }
+    : {
+        cssMinify: false,
+        minify: false,
+      };
+
+  return {
+    plugins: [tsConfigPaths(), react()],
+    envDir: './env',
+    build: {
+      ...productionConfig,
+      emptyOutDir: true,
+      rollupOptions: {
+        treeshake: true,
+      },
+      lib: {
+        entry: 'src/index.ts',
+        name: '@acap/ui-react',
+        fileName: 'acap-ui-react',
+        formats: ['es'] as any,
+      },
     },
     css: {
       modules: {
-        classNameStrategy: 'stable',
+        localsConvention: 'camelCase' as any,
       },
     },
-  },
-}));
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './test-setup.ts',
+      coverage: {
+        reporter: 'json',
+      },
+      css: {
+        modules: {
+          classNameStrategy: 'stable',
+        },
+      },
+    },
+  };
+});
