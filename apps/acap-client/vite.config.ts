7ac5393703f942393803b2cbf8ec09678df2fbde
Eugen Hildt <eugen.hildt@litlbyte.com>
2023-10-08 16:44:16 +0200

    chore: updated vite.config.ts (build)

--- apps/acap-client/vite.config.ts
+++ apps/acap-client/vite.config.ts
@@ -2,25 +2,45 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(() => ({
-  plugins: [tsConfigPaths(), react()],
-  envDir: './env',
-  css: {
-    modules: {
-      localsConvention: 'camelCase' as any,
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './test-setup.ts',
-    coverage: {
-      reporter: 'json',
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+  const productionConfig = isProduction
+    ? {
+        cssMinify: true,
+        minify: true,
+      }
+    : {
+        cssMinify: false,
+        minify: false,
+      };
+
+  return {
+    plugins: [tsConfigPaths(), react()],
+    envDir: './env',
+    build: {
+      ...productionConfig,
+      emptyOutDir: true,
+      rollupOptions: {
+        treeshake: true,
+      },
     },
     css: {
       modules: {
-        classNameStrategy: 'stable',
+        localsConvention: 'camelCase' as any,
       },
     },
-  },
-}));
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './test-setup.ts',
+      coverage: {
+        reporter: 'json',
+      },
+      css: {
+        modules: {
+          classNameStrategy: 'stable',
+        },
+      },
+    },
+  };
+});
