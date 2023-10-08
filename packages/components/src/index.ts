5ac358f5fbe905f7cde186f38b8d6293af2a039e
Eugen Hildt <eugen.hildt@litlbyte.com>
2023-10-08 18:59:48 +0200

    chore: added file-selector to components

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,4 +1,5 @@
 export { Button } from '@/button/Button';
+export { FileSelector } from '@/file-selector/FileSelector';
 export { PageMenu } from '@/page-menu/PageMenu';
 export { PageMenuItem } from '@/page-menu/PageMenuItem';
 export { PageSwitcher } from '@/page-switcher/PageSwitcher';
