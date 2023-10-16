324b75919bace879496e08eb7a8e6a095e9908d3
Eugen Hildt <eugen.hildt@litlbyte.com>
2023-10-16 13:44:38 +0200

    feat: enhanced the crypto services encryption & decryption logic

--- apps/backend/src/constants/app.constants.ts
+++ apps/backend/src/constants/app.constants.ts
@@ -1,9 +1,11 @@
 export const API_DOCS = 'api-docs';
-export const API_DOCS_JSON = 'api-docs-json';
-
-export const REDIS_PUBSUB = 'REDIS_PUBSUB';
-
-export const BULLMQ_REALM_QUEUE = 'ACAP_REALM_QUEUE';
-
-export const SCHEMA_PREFIX = 'SCHEMA';
 export const REALM_PREFIX = 'REALM';
+export const SCHEMA_PREFIX = 'SCHEMA';
+export const REDIS_PUBSUB = 'REDIS_PUBSUB';
+export const API_DOCS_JSON = 'api-docs-json';
+export const BULLMQ_REALM_QUEUE = 'ACAP_REALM_QUEUE';
+export const ALGORITHM = {
+  16: 'aes-128-cbc',
+  24: 'aes-192-cbc',
+  32: 'aes-256-cbc',
+};
