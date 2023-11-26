b01aa6c344b30d5c42fa7c45c237895f7ab287a7
Eugen Hildt <eugen.hildt@litlbyte.com>
2023-11-26 12:31:13 +0100

    ui: updated FileImporter

--- packages/ui/src/file-importer/FileImporterContentList.tsx
+++ packages/ui/src/file-importer/FileImporterContentList.tsx
@@ -1,7 +1,10 @@
-import { useLayoutEffect, useRef } from 'react';
+import { useRef, useState } from 'react';
 import { FaFile, FaFileCsv, FaFileExcel, FaFilePdf, FaFileWord, FaRegFileImage } from 'react-icons/fa6';
 
+import { ImageViewer } from '@/file-viewers/image-viewer/ImageViewer';
+
 import { PageMenu, PageMenuItem, Scrollbar } from '..';
+import { PdfViewer } from '../file-viewers/pdf-viewer/PdfViewer';
 import { useImageRenderer } from './FileImporter.hooks';
 
 type SUPPORTED_EXTENSIONS = 'pdf' | 'csv' | 'xlsx' | 'odt' | 'docx' | 'jpg' | 'png';
@@ -27,23 +30,32 @@ type PropsFileImporterContentList = {
 };
 
 export function FileImporterContentList(props: PropsFileImporterContentList) {
+  const [file, setFile] = useState<any>();
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const preRef = useRef<HTMLParagraphElement>(null);
 
-  const items = props.files.map((f, idx) => (
-    <PageMenuItem key={idx} onClick={() => useImageRenderer(f, canvasRef)}>
-      <div className="file-card">
-        {mapFileExtensionToIcon(f.extension)} {f.name} <br />
-        {f.size} {f.lastModified}
-      </div>
-    </PageMenuItem>
-  ));
-
-  useLayoutEffect(() => {
-    if (props.files?.length) {
-      const file = props.files.find((f) => f.extension === 'jpg');
-      if (file) useImageRenderer(file, canvasRef);
-    }
-  }, []);
+  const items = props.files?.map((f, idx) => {
+    return (
+      <PageMenuItem
+        key={idx}
+        onClick={() => {
+          setFile(f);
+          if (f.extension !== 'txt' && f.extension !== 'json') {
+            useImageRenderer(f, canvasRef);
+          } else {
+            if (f.extension === 'json' || f.extension === 'txt') {
+              preRef.current!.innerText = f.buffer.toString('utf8');
+            }
+          }
+        }}
+      >
+        <div className="file-card">
+          {mapFileExtensionToIcon(f.extension)} {f.name} <br />
+          {f.size} {f.lastModified}
+        </div>
+      </PageMenuItem>
+    );
+  });
 
   return (
     <div className="file-importer-content">
@@ -52,8 +64,16 @@ export function FileImporterContentList(props: PropsFileImporterContentList) {
           <PageMenu>{items}</PageMenu>
         </Scrollbar>
       </div>
-
-      <canvas ref={canvasRef} className="file-importer-content-preview" itemType="*/*" />
+      <div className="file-importer-content-tags">tags are mine</div>
+      <div className="file-importer-content-preview">
+        <Scrollbar>
+          {file?.extension !== 'pdf' && file?.extension !== 'jpg' && <p ref={preRef} />}
+          {file?.extension === 'jpg' && (
+            <ImageViewer mimeType={file.mimeType} base64={file.buffer.toString('base64')} />
+          )}
+          {file?.extension === 'pdf' && <PdfViewer base64={file.buffer.toString('base64')} scale={0.59} />}
+        </Scrollbar>
+      </div>
     </div>
   );
 }
