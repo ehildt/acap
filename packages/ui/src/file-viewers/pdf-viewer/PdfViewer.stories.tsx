3ab861c8c3d5073776d31cb2ca5554ca103d73ee
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-02-04 20:57:38 +0100

    ui: updated TreeViewer component

--- packages/ui/src/file-viewers/pdf-viewer/PdfViewer.stories.tsx
+++ packages/ui/src/file-viewers/pdf-viewer/PdfViewer.stories.tsx
@@ -6,14 +6,13 @@ import { PdfViewer } from './PdfViewer';
 export default {
   title: 'viewers/pdf-viewer',
   component: PdfViewer,
-  decorators: [(renderer) => <div style={{ width: 'fit-content' }}>{renderer()}</div>],
+  decorators: [(renderer) => <div>{renderer()}</div>],
 } satisfies Meta<typeof PdfViewer>;
 
 export const DefaultPdfViewer = {
   render: () => (
     <PdfViewer
       base64={pdfBase64Example}
-      scale={0.5}
       formatter={(currentPageNumber, totalPageCount) => `Page ${currentPageNumber} of ${totalPageCount}`}
     />
   ),
