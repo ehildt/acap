ba2bae90b819956b9a2655e7c32f153896e0aa23
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-02-04 21:02:22 +0100

    ui: wip on FileImporter

--- packages/ui/src/file-selector/FileSelector.tsx
+++ packages/ui/src/file-selector/FileSelector.tsx
@@ -10,18 +10,21 @@ export function FileSelector(props: FileSelectorProps) {
   const inputRef = useRef<any>(null);
 
   return (
-    <div className="file-selector" onClick={() => inputRef.current?.click()}>
-      <FaFileImport size={'2rem'} />
-      <label htmlFor={props.label}>{props.label}</label>
-      <input
-        type="file"
-        ref={inputRef}
-        name={props.label}
-        style={{ display: 'none' }}
-        accept={props.accept}
-        onChange={useChangeEventProxy(props.onChange)}
-        multiple
-      />
+    <div style={{ display: 'flex', gap: '0.5rem' }}>
+      <div className="file-selector" onClick={() => inputRef.current?.click()}>
+        <FaFileImport size={'2rem'} />
+        <label htmlFor={props.label}>{props.label}</label>
+        <input
+          type="file"
+          ref={inputRef}
+          name={props.label}
+          style={{ display: 'none' }}
+          accept={props.accept}
+          onChange={useChangeEventProxy(props.onChange)}
+          multiple
+        />
+      </div>
+      <div className="file-menu">file menu here</div>
     </div>
   );
 }
