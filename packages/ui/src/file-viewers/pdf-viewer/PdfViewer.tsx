94b94b433948d203a7b56cc7fb2cec6d8e2e9788
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-02-04 20:57:38 +0100

    ui: updated TreeViewer component

--- packages/ui/src/file-viewers/pdf-viewer/PdfViewer.tsx
+++ packages/ui/src/file-viewers/pdf-viewer/PdfViewer.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa6';
 import { Document, Page, pdfjs } from 'react-pdf';
 
+import { Container } from '@/container/Container';
+
 import style from './PdfViewer.module.scss';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
@@ -19,26 +21,28 @@ export function PdfViewer(props: PdfViewerProps) {
   const [currentPage, setCurrentPage] = useState<number>(1);
 
   return (
-    <div className={style.pdfViewer}>
-      <div className={style.pdfViewerContainer}>
-        <Document
-          file={`data:application/pdf;base64,${props.base64}`}
-          onLoadSuccess={({ numPages }) => {
-            setNumPages(numPages);
-          }}
-        >
-          {pages && <Page pageNumber={currentPage} scale={props.scale ?? 1} renderAnnotationLayer={false} />}
-        </Document>
+    <Container>
+      <div className={style.pdfViewer}>
+        <div className={style.pdfViewerContainer}>
+          <Document
+            file={`data:application/pdf;base64,${props.base64}`}
+            onLoadSuccess={({ numPages }) => {
+              setNumPages(numPages);
+            }}
+          >
+            {pages && <Page pageNumber={currentPage} renderAnnotationLayer={false} />}
+          </Document>
+        </div>
+        <div className={style.pdfViewerButtonMenu}>
+          <button disabled={currentPage <= 1} onClick={() => setCurrentPage((val) => val - 1)}>
+            <FaChevronLeft size={'1.5rem'} />
+          </button>
+          <p>{props.formatter?.(currentPage, pages) ?? `Page ${currentPage} of ${pages}`}</p>
+          <button disabled={currentPage >= pages} onClick={() => setCurrentPage((val) => val + 1)}>
+            <FaChevronRight size={'1.5rem'} />
+          </button>
+        </div>
       </div>
-      <div className={style.pdfViewerButtonMenu}>
-        <button disabled={currentPage <= 1} onClick={() => setCurrentPage((val) => val - 1)}>
-          <FaChevronLeft size={'1.5rem'} />
-        </button>
-        <p>{props.formatter?.(currentPage, pages) ?? `Page ${currentPage} of ${pages}`}</p>
-        <button disabled={currentPage >= pages} onClick={() => setCurrentPage((val) => val + 1)}>
-          <FaChevronRight size={'1.5rem'} />
-        </button>
-      </div>
-    </div>
+    </Container>
   );
 }
