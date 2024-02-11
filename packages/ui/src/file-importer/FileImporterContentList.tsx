a4818787fe8465b02d1198e0740e5147a00081a6
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-02-11 07:54:30 +0100

    ui: improved TreeViewer and fixed resizing bug

--- packages/ui/src/file-importer/FileImporterContentList.tsx
+++ packages/ui/src/file-importer/FileImporterContentList.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { FaFile, FaFileCsv, FaFileExcel, FaFilePdf, FaFileWord, FaRegFileImage } from 'react-icons/fa6';
 import { parse } from 'yaml';
 
@@ -32,7 +32,7 @@ type PropsFileImporterContentList = {
 export function FileImporterContentList(props: PropsFileImporterContentList) {
   const fileSlice = useFileImporterImmerStore();
   const [file, setFile] = useState<any>();
-  const ref = useRef(null);
+
   const items = props.files?.map((f, idx) => {
     return (
       <PageMenuItem key={idx} onClick={() => setFile(f)}>
@@ -51,14 +51,12 @@ export function FileImporterContentList(props: PropsFileImporterContentList) {
           <PageMenu>{items}</PageMenu>
         </Scrollbar>
       </div>
-      <div ref={ref} className="file-importer-content-preview">
-        <Scrollbar overflow={fileSlice.toggleTreeView ? 'hidden' : 'auto'}>
+      <div className="file-importer-content-preview">
+        <Scrollbar>
           {file?.extension === 'json' && fileSlice.toggleTreeView && (
-            <TreeViewer object={JSON.parse(file.buffer.toString())} ref={ref} />
+            <TreeViewer data={JSON.parse(file.buffer.toString())} />
           )}
-          {file?.extension === 'yml' && fileSlice.toggleTreeView && (
-            <TreeViewer object={parse(file.buffer.toString())} ref={ref} />
-          )}
+          {file?.extension === 'yml' && fileSlice.toggleTreeView && <TreeViewer data={parse(file.buffer.toString())} />}
           {file?.extension === 'json' && !fileSlice.toggleTreeView && <JsonViewer json={file.buffer.toString()} />}
           {file?.extension === 'yml' && !fileSlice.toggleTreeView && <YmlViewer yml={parse(file.buffer.toString())} />}
           {file?.extension === 'pdf' && <PdfViewer base64={file.buffer.toString('base64')} />}
