f26e64dc0c0a95139a9fad84fec75c55662d6efe
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-03-23 20:53:53 +0100

    fix: fixed ts-unused-exports

--- packages/ui/src/button/Button.hooks.tsx
+++ packages/ui/src/button/Button.hooks.tsx
@@ -1,23 +1,4 @@
-import cn from 'classnames';
-
-import { ButtonProps, ProxyFunc } from './Button.modal';
-import style from './Button.module.scss';
-
-export function useIconBefore(props: ButtonProps) {
-  if (props.iconBefore) return <span className={useButtonIconBeforeCn(props)}>{props.iconBefore}</span>;
-}
-
-export function useIconAfter(props: ButtonProps) {
-  if (props.iconAfter) return <span className={useButtonIconAfterCn(props)}>{props.iconAfter}</span>;
-}
-
-function useButtonIconBeforeCn(props: ButtonProps) {
-  return cn([{ [style.buttonIconBefore]: props.iconBefore }, { [style.buttonIconBeforeMarginRight]: props.children }]);
-}
-
-function useButtonIconAfterCn(props: ButtonProps) {
-  return cn([{ [style.buttonIconAfter]: props.iconAfter }, { [style.buttonIconBeforeMarginLeft]: props.children }]);
-}
+import { ProxyFunc } from './Button.modal';
 
 export function useMouseEventProxy(callback?: ProxyFunc) {
   return (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
